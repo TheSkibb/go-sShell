f198882ef96b8cd32f7ce5476f3f8399fb0f1049
theskibb <kristianskibrek@protonmail.com>
2025-06-03 11:14:29 +0200

    poc

--- examples/calculator/main.go
+++ examples/calculator/main.go
@@ -3,8 +3,74 @@ package main
 import (
 	"fmt"
 	"github.com/theskibb/sShell/sShell"
+	"strconv"
 )
 
 func main() {
-	fmt.Println(sshell.Example())
+	fmt.Println()
+
+	s := sshell.ShellSettings{
+		Promt:   ">",
+		ExitMsg: "exit",
+		Commands: []sshell.Command{
+			{
+				Input:   "add",
+				Handler: add,
+				HelpMsg: "add <arg1> <arg2> <arg3> ...",
+			},
+			{
+				Input:   "sub",
+				Handler: subtract,
+				HelpMsg: "sub <arg1> <arg2> <arg3> ...",
+			},
+		},
+	}
+
+	sshell.StartShell(s)
 }
+
+func add(args []string) string {
+
+	total := 0
+
+	for _, arg := range args {
+		num, err := strconv.Atoi(arg)
+		if err != nil {
+			return arg + "is not a number"
+		}
+
+		total += num
+	}
+
+	totalStr := strconv.Itoa(total)
+
+	return totalStr
+}
+
+func subtract(args []string) string {
+
+	total := 0
+
+	for i, arg := range args {
+		num, err := strconv.Atoi(arg)
+		if err != nil {
+			return arg + "is not a number"
+		}
+		if i == 0 {
+			total = num
+		} else {
+			total -= num
+		}
+
+	}
+
+	totalStr := strconv.Itoa(total)
+
+	return totalStr
+}
+
+/*
+func calc(args []string) string {
+
+}
+*/
